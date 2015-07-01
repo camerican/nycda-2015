66ebb35d85a5bf83d9641899831aec7874f94628
Cam Crews <cam@nycda.com>
2015-07-01 11:37:24 -0400

    Form App Update
    Paired Programming Ruby Update

---
+++ formapp/app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './models'
+
+set :database, "sqlite3:flash.sqlite3"
+
+set :sessions, true
+
+post '/register' do
+  User.create(params)
+
+  @users = User.all
+
+  erb :dashboard
+end
+
+get '/register' do
+  erb :register
+end
+
+
+get '/pair' do
+  erb :pair_programming
+end
+
+get '/login' do
+  erb :form
+end
+
+post '/login' do
+  @user = User.where(email: params[:email]).first
+  if @user.password == params[:password]
+     redirect '/'
+  else
+     redirect '/login-failed'
+  end
+end
